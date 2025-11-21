d1aae87fb132650c4c056cd69e9e9cacbc9061dd
Hacariiiii <hebamouad1234gmail.com>
2025-11-21 11:56:22 +0100

    sauvegarde 1

---
+++ catalogue-service/src/main/java/com/example/catalogueservice/CatalogueServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.catalogueservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class CatalogueServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogueServiceApplication.class, args);
+	}
+
+}
